2ec6e55eaa9b12e7197e1dc592f85a34e113d723
zc2513 <yyzc2513@163.com>
2020-01-03 16:49:34 +0800

    .

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import '@/styles/index.scss' // global css
-import '@/styles/element-variables.scss' // global css
+import '@/styles/element-variables.scss' // element-ui 全局样式文件
 
 import App from './App'
 import store from './store'
